6555e50672d7f5bbc09b64a90e0e625e6dad06f4
T. Sheng <34408561+tsheng0315@users.noreply.github.com>
2023-02-01 01:05:46 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Menu')
-streamlit.text('Blueberry')
-streamlit.text('Kale')
-streamlit.text('Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
